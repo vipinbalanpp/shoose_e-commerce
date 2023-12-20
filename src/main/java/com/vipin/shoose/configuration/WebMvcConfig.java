c950c23aae8e384bf156a4faaf35cf50617a0221
vipinbalanpp <vipinbalanpp@gmail.com>
2023-12-20 17:58:35 +0530

    changes made

--- src/main/java/com/vipin/shoose/configuration/WebMvcConfig.java
+++ src/main/java/com/vipin/shoose/configuration/WebMvcConfig.java
@@ -1,8 +1,11 @@
 package com.vipin.shoose.configuration;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
