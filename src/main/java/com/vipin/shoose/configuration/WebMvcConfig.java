10122825ccbc7dd3145c54dcbad6a5fbf33a5061
vipinbalanpp <vipinbalanpp@gmail.com>
2023-12-20 18:03:38 +0530

    changes made

--- src/main/java/com/vipin/shoose/configuration/WebMvcConfig.java
+++ src/main/java/com/vipin/shoose/configuration/WebMvcConfig.java
@@ -10,6 +10,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/img/product-images/**")
-                .addResourceLocations("/home/ubuntu/prod/src/main/resources/static/img/product-images/");
+                .addResourceLocations("file:/home/ubuntu/prod/src/main/resources/static/img/product-images/");
     }
 }
